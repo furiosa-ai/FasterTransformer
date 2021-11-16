79bdd371ea37be651c3406108d8cd47412937418
ByungkeonKo <ccrspeed@furiosa.ai>
2021-11-16 15:12:40 +0900

    generate GPT script for easier test.

---
+++ sample/pytorch/gen_sample_input_file.py
@@ -0,0 +1,52 @@
+import argparse
+
+import utils.gpt_token_encoder as encoder
+import torch
+import numpy as np
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch_size', type=int, default=8,
+                        help='batch size.')
+    parser.add_argument('--input_seq_len', type=int, default=24,
+                        help='number of layers')
+    parser.add_argument('--o_file_name', type=str, default='./sample_input.txt',
+                        help='path to the sample text file.')
+    parser.add_argument('--vocab_file', type=str, default="../models/gpt2-vocab.json",
+                        help='vocabulary file.')
+    parser.add_argument('--merges_file', type=str, default="../models/gpt2-merges.txt",
+                        help='merges file.')
+
+    args = parser.parse_args()
+
+    batch_size = args.batch_size
+    input_seq_len = args.input_seq_len
+    o_file_name = args.o_file_name
+
+    print("\n=============== Arguments ===============")
+    for arg in vars(args):
+        print ("{}: {}".format(arg, getattr(args, arg)))
+    print("=========================================\n")
+
+    with open( "source_sentence.txt", "r" ) as fp :
+        src_sentence = fp.read()
+
+    enc = encoder.get_encoder(args.vocab_file, args.merges_file)
+
+    contexts = [ src_sentence ]
+    start_ids = [torch.IntTensor( enc.encode(c)) for c in contexts]
+    start_ids = np.array( start_ids[0] )
+
+    start_ids = start_ids[:input_seq_len]
+
+    output = enc.decode( start_ids )
+
+    with open( o_file_name, "w" ) as fp :
+        for _ in range( batch_size ) :
+            fp.write( output + '\n' )
+
+    return
+
+if __name__ == '__main__':
+    main()
+
---
+++ sample/pytorch/scripts/mgwg_gpt_sample.run.sh
@@ -0,0 +1,61 @@
+# ex) ./pytorch/scripts/mgwg_gpt_sample.run.sh 125M 256 32 8 8 1 1 1
+
+# $1 = <model_name>
+# $2 = max input sequence length ( S1 )
+# $3 = max output sequence length ( S2 )
+# $4 = batch_size
+# $5 = <local_batch_size>
+# $6 = tensor_para_size
+# $7 = layer_para_size
+# $8 = n_gpu
+
+if [ $# -lt 7 ]; then
+    echo "More arguments are needed."
+    exit 1
+fi
+
+model_name=$1
+s1=$2
+s2=$3
+batch_size=$4
+local_batch_size=$5
+context_local_batch_size=$local_batch_size
+tensor_para_size=$6
+layer_para_size=$7
+n_gpu=$8
+
+max_seq_len=$(($s1+$s2))
+
+if [ $model_name == "125M" ]; then
+    head_num=12
+    size_per_head=64
+    vocab_size=50257
+    ckpt_path=../models/openai-gpt-models/c-model/124m/1-gpu
+    layer_num=12
+elif [ $model_name == "1558M" ]; then
+    head_num=25
+    size_per_head=64
+    vocab_size=50257
+    ckpt_path=../models/openai-gpt-models/c-model/1558m/1-gpu
+    layer_num=48
+elif [ $model_name == "test" ]; then
+    head_num=25
+    size_per_head=64
+    vocab_size=50257
+    ckpt_path=-1
+    layer_num=48
+else
+    echo "wrong model name"
+    exit 1
+fi
+
+is_fp16=1
+
+# echo $ckpt_path
+# echo $max_seq_len
+
+# ./bin/gpt_gemm $local_batch_size $context_local_batch_size $head_num $size_per_head $vocab_size $s1 $tensor_para_size $is_fp16
+# python ./pytorch/gen_sample_input_file.py --batch_size $batch_size --input_seq_len $s1 --o_file_name ./sample_input.txt
+
+mpirun -n $n_gpu --allow-run-as-root python ./pytorch/gpt_sample.py --ckpt_path $ckpt_path --fp16 --head_num $head_num --layer_num $layer_num --vocab_size $vocab_size --output_len $s2 --max_seq_len $max_seq_len --top_k 0 --top_p 0.9 --sample_input_file ./sample_input.txt
+
